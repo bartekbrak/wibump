4a75c01bd183bc7c746cb62ff0f1225d3911eadd
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-05-13 16:51:20 +0200

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='wibump',
-    version='1.1.0',
+    version='1.1.1',
     py_modules=['wibump'],
     entry_points={'console_scripts': ['bump=wibump:main']}
 )
