beb3a43211f9fa647ba9a379d2b278d6d97d399a
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-05-25 16:10:11 +0200

    Correct colours.

--- wibump.py
+++ wibump.py
@@ -60,7 +60,7 @@ def pprint(msg, level):
     if level == 'info':
         print('[{t.yellow}INFO{t.normal}] {msg}'.format(msg=msg, t=t))
     elif level == 'called':
-        print('[{t.bright_red}CALLED{t.normal}] {msg}'.format(msg=msg, t=t))
+        print('[{t.bold_red}CALLED{t.normal}] {msg}'.format(msg=msg, t=t))
 
 info = partial(pprint, level='info')
 called = partial(pprint, level='called')
