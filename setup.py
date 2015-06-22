d55cc6fc73dadef84c0c578c323834987f062b32
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-06-22 16:00:12 +0200

    Bump version to 1.2.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wibump',
-    version='1.2.8',
+    version='1.2.9',
     py_modules=['wibump'],
     install_requires=['GitPython', 'blessings'],
     entry_points={'console_scripts': ['bump=wibump:main']}
